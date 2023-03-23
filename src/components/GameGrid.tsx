36128176c1a5240fea2deb09dc3d21d620592053
Yusuff Ridwan <wemzain@gmail.com>
2023-03-23 21:31:05 +0100

    Built sort selector

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -19,7 +19,7 @@ const GameGrid = ({ gameQuery }: Props) => {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="16px"
+        padding={2}
         spacing={4}
       >
         {isLoading &&
