fbc35a4b15a52f982f0860e75006db319d37075c
Yusuff Ridwan <wemzain@gmail.com>
2023-03-23 11:04:11 +0100

    Worked on refining the genre list

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,7 +13,7 @@ const GameGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="16px"
-        spacing={10}
+        spacing={4}
       >
         {isLoading &&
           skeletons.map((s) => (
--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,15 +1,26 @@
+import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import useData from "../hooks/useData";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-services";
 
 const GenresList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY={"5px"}>
+          <HStack>
+            <Image
+              boxSize={"32px"}
+              src={getCroppedImageUrl(genre.image_background)}
+              borderRadius={8}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
