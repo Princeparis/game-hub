581f84d9aec7137827b97c60016fd639e9ba4dad
Yusuff Ridwan <wemzain@gmail.com>
2023-06-24 22:36:07 +0100

    Simplified time calc

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import { Platform } from "./usePlatforms";
 import APIClient, { FetchResponse } from "../services/api-client";
+import ms from "ms";
 
 // export interface Platform {
 //   id: number;
@@ -35,7 +36,7 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allpages) => {
       return lastPage.next ? allpages.length + 1 : undefined;
     },
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms("24h"),
   });
 
 export default useGames;
