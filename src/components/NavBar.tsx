69bf2a3a2b77742cdf8d202eeead0f55dffb6476
Yusuff Ridwan <wemzain@gmail.com>
2023-07-08 00:56:15 +0100

    Fixed Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,16 @@
 import { HStack, Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-
-
 const NavBar = () => {
   return (
     <HStack justifyContent={"space-between"} paddingX={"10px"} paddingY={4}>
-      <Image src={logo} alt="Logo" boxSize={"60px"} />
-      <SearchInput  />
+      <Link to={"/"}>
+        <Image src={logo} alt="Logo" boxSize={"60px"} objectFit="cover" />
+      </Link>
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
